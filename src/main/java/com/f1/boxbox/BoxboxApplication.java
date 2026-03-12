33653331ee4f8a5c030002b56bb4876688bb8be8
Rafael <librerorafael@outlook.com>
2026-03-12 11:12:53 +0100

    First commit

---
+++ src/main/java/com/f1/boxbox/BoxboxApplication.java
@@ -0,0 +1,13 @@
+package com.f1.boxbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxboxApplication.class, args);
+	}
+
+}
